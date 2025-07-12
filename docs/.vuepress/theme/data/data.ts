a080927518a2bd9d9d4616fdd6a8c49a52da29e4
pinkdopeybug <pinkdopeybug@163.com>
2025-07-12 10:59:31 +0800

    vault backup: 2025-07-12 10:59:31

--- docs/.vuepress/theme/data/data.ts
+++ docs/.vuepress/theme/data/data.ts
@@ -1,24 +1,11 @@
 // 朋友
 export const friends = [
   {
-    name: "一大口木",
-    link: "https://blog.csdn.net/m0_71274136?ops_request_misc=%7B%22request%5Fid%22%3A%22a2843a4819104e972d40d40e7ec76377%22%2C%22scm%22%3A%2220140713.130064515..%22%7D&request_id=a2843a4819104e972d40d40e7ec76377&biz_id=206&utm_medium=distribute.pc_search_result.none-task-user-null-1-155244413-null-null.nonecase&utm_term=%E4%B8%80%E5%A4%A7%E5%8F%A3%E6%9C%A8&spm=1018.2226.3001.4351",
-    avatar:
-      "https://profile-avatar.csdnimg.cn/5f555d1f54aa47b5b6859aa01ad8b8cf_m0_71274136.jpg!1",
-    desc: "山信的，linux，python，web，c++，html，ensp，网络安全",
-  },
-  {
-    name: "czpkfc",
-    link: "https://github.com/czpkfc",
-    avatar: "https://avatars.githubusercontent.com/u/88567456?v=4",
-    desc: "机会是留给有准备的人的，所以没机会别瞎准备——丁真",
-  },
-  {
-    name: "niuniu-02",
-    link: "https://github.com/niuniu-02",
-    avatar: "https://avatars.githubusercontent.com/u/217345475?v=4",
-    desc: "往事暗沉不可追，来日之路光明灿烂",
-  },
+    name: "Hoey",
+    link: "https://zhenghaoyang.cn/",
+    avatar:"https://zhenghaoyang.cn/avatar.jpg",
+    desc: "老师，我太想进步了。",
+  }
 ];
 
 // 性格
