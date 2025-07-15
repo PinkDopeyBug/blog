1874b238d9c582daa9fee8ef2f2071fb0dcba2c7
pinkdopeybug <pinkdopeybug@163.com>
2025-07-15 21:31:27 +0800

    t3

--- api/data.ts
+++ api/data.ts
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 const site_id = 22211231;
 const access_token =
   "121.0a3259e92647f2426386a37cc1a6a427.YlI4NfVF9GptzSoZGMUqGIC0uF1pngwr9X3JAXe.XD6knQ";
@@ -10,9 +9,6 @@ function formatDate(date) {
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}${month}${day}`;
 }
-module.exports = (req, res) => {
-  const end_date = formatDate(new Date());
-  const url = `https://openapi.baidu.com/rest/2.0/tongji/report/getData?access_token=${access_token}&site_id=${site_id}&method=visit/toppage/a&start_date=${start_date}&end_date=${end_date}&metrics=pv_count,visitor_count`;
-
+export default (req, res) => {
   res.status(200).json({ msg: "获取成功" });
 };
